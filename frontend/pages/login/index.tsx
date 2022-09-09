0f73ae5b8144e836f6a898fe92f2096b1dd50337
abdlalisalmi <abdlalisalmi@gmail.com>
2022-09-09 23:31:19 +0100

    init the frontend

---
+++ frontend/pages/login/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import styles from '../../styles/Login.module.css'
+
+const Login: NextPage = () => {
+  return (
+	  <div className={styles.loginCard}>
+		<h1>Login</h1>
+		<form>
+			<input type="text" name="" id="" />
+			<input type="password" name="" id="" />
+		</form>
+	  </div>
+  )
+}
+
+export default Login
