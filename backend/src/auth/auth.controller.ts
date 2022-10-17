e9d4b94f908d2743520ce8767c67aa362fff6548
atah1r1 <atahiri@student.1337.ma>
2022-10-18 00:34:56 +0100

    ok

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -18,7 +18,7 @@ export class AuthController {
         // }
         const jwt = await this.AuthService.Login(username, name, photos);
         res.cookie('jwt', jwt);
-        return { jwt };
+        return res.redirect('http://localhost:3000/settings/profile');
     }
 
     @Get('logout')
