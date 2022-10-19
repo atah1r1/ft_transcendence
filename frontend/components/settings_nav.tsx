8ad3b8cce3d8aba958ba50cb871d9216cad42e10
Yacine Hadari <yhadari@e2r7p4.1337.ma>
2022-10-19 21:31:36 +0100

    modify settings style

--- frontend/components/settings_nav.tsx
+++ frontend/components/settings_nav.tsx
@@ -2,6 +2,7 @@ import cn from "classnames";
 import { useRouter } from "next/router";
 import styles_box from "../styles/style_box.module.css";
 import styles_s_l from "../styles/style_settings_nav.module.css";
+import Image from "next/image";
 
 const SettingsNav = ({ selected }: any) => {
   const sections = ["profile", "chat", "history", "statistics", "friends"];
@@ -9,11 +10,13 @@ const SettingsNav = ({ selected }: any) => {
   return (
     <div className={styles_box.profile_setting}>
       <div className={styles_s_l.profile_info}>
-        <img
+        <Image
           src="https://cdn.intra.42.fr/users/yhadari.jpg"
-          width="30%"
+          alt="avatar"
+          width={90}
+          height={90}
           className={styles_s_l.profile_image}
-        ></img>
+        ></Image>
         <p className={styles_s_l.profile_info_login}>YHADARI</p>
         <p className={styles_s_l.profile_info_full_name}>YACINE HADARI</p>
       </div>
