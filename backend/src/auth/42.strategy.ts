a128cb6fa8549634775a646501d936316da0e146
atah1r1 <atahiri@student.1337.ma>
2022-10-19 21:29:16 +0100

    Merge to master

--- backend/src/auth/42.strategy.ts
+++ backend/src/auth/42.strategy.ts
@@ -11,7 +11,7 @@ export class FortyTwoStrategy extends PassportStrategy(Strategy, '42') {
     super({
       clientID: process.env.FORTYTWO_CLIENT_ID,
       clientSecret: process.env.FORTYTWO_CLIENT_SECRET,
-      callbackURL: 'http://10.11.8.10:9000/api/auth/redirect',
+      callbackURL: 'http://localhost:9000/api/auth/redirect',
     });
   }
 
