875cfeb04db044760223b55ef3d628ad87d603f4
atahiri <atahiri@student.1337.ma>
2022-11-04 01:08:28 +0100

    check cookies

--- frontend/pages/settings/profile/index.tsx
+++ frontend/pages/settings/profile/index.tsx
@@ -8,128 +8,174 @@ import { UserContext } from "../../_app";
 import axios from "axios";
 import Image from "next/image";
 import Loader from "../../../components/Loading";
+import { useRouter } from "next/router";
+import cookie from 'cookie';
+import requireAuthentication from "../../../hooks/requiredAuthentication";
 
-const Profile = () =>
-{
-  const context = useContext( UserContext );
-  const isLogin = context?.data.id ? true : false;
-  console.log( context?.data );
-  const [ saveBtn, setSaveBtn ] = useState( false );
-  const [ s_witch, setSwitch ] = useState( false );
+const Profile = () => {
+
+  const [data, setData] = useState(
+    {
+      avatar: "https://cdn.intra.42.fr/users/yhadari.jpg",
+      createdAt: "",
+      first_name: "",
+      id: "",
+      last_name: "",
+      two_factor_auth: false,
+      updateAt: "",
+      username: "",
+    }
+  );
+  const [loader, setLoader] = useState(true);
+  const [saveBtn, setSaveBtn] = useState(false);
+  const [s_witch, setSwitch] = useState(false);
+
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/user/me`,
+        {
+          withCredentials: true,
+        }
+      );
+      setData(response.data);
+      localStorage.setItem("user", JSON.stringify(response.data));
+    } catch (error: any) {
+    }
+    finally {
+      setLoader(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  })
+
+
+  console.log(data);
 
   return (
-    isLogin ? <div className={ styles_box.container }>
-      <div className={ cn( styles_s_l.setting_btn, styles_s_l.current_btn, styles_s_l.logout_btn ) }>logout</div>
-      <SettingsNav selected={ "profile" } />
-      <div className={ styles_box.profile_details }>
+    <div className={styles_box.container}>
+      <div className={cn(styles_s_l.setting_btn, styles_s_l.current_btn, styles_s_l.logout_btn)}>logout</div>
+      <SettingsNav selected={"profile"} />
+      <div className={styles_box.profile_details}>
         {
-          context?.loader ? <Loader /> :
-            <div>
-              <div className={ styles.details_up }>
-                <div className={ styles.details_level }>
-                  <p>LEVEL</p>
-                  <span> 2</span>
+          loader ? <Loader /> :
+          <div>
+            <div className={styles.details_up}>
+              <div className={styles.details_level}>
+                <p>LEVEL</p>
+                <span> 2</span>
+              </div>
+              <div className={styles.details_avatar}>
+                <div className={styles.upload_avatar}>
+                  <Image
+                    src="/upload_avatar.png"
+                    alt="upload_avatar_img"
+                    width="100%"
+                    height="100%"
+                  ></Image>
                 </div>
-                <div className={ styles.details_avatar }>
-                  <div className={ styles.upload_avatar }>
-                    <Image
-                      src="/upload_avatar.png"
-                      alt="upload_avatar_img"
-                      width="100%"
-                      height="100%"
-                    ></Image>
-                  </div>
-                  <div className={ styles.profile_box }>
-                    <div className={ styles.profile_slide }>change picture</div>
-                    <input type="file"></input>
-                    <Image
-                      src={ context!.data.avatar }
-                      alt="avatar"
-                      width="180px"
-                      height="180px"
-                    ></Image>
-                  </div>
-                  <p>{ context!.data.username }</p>
+                <div className={styles.profile_box}>
+                  <div className={styles.profile_slide}>change picture</div>
+                  <input type="file"></input>
+                  <Image
+                    src={data.avatar}
+                    alt="avatar"
+                    width="180px"
+                    height="180px"
+                  ></Image>
                 </div>
-                <div className={ styles.details_medals }>
-                  <div>
-                    <Image
-                      src="/ach1.png"
-                      alt="medal_img"
-                      width="40%"
-                      height="40%"
-                    ></Image>
-                  </div>
-                  <div>
-                    <Image
-                      src="/ach2.png"
-                      alt="medal_img"
-                      width="40%"
-                      height="40%"
-                    ></Image>
-                  </div>
-                  <div>
-                    <Image
-                      src="/ach3.png"
-                      alt="medal_img"
-                      width="40%"
-                      height="40%"
-                    ></Image>
-                  </div>
-                  <div>
-                    <Image
-                      src="/ach4.png"
-                      alt="medal_img"
-                      width="40%"
-                      height="40%"
-                    ></Image>
-                  </div>
+                <p>{data.username}</p>
+              </div>
+              <div className={styles.details_medals}>
+                <div>
+                  <Image
+                    src="/ach1.png"
+                    alt="medal_img"
+                    width="40%"
+                    height="40%"
+                  ></Image>
+                </div>
+                <div>
+                  <Image
+                    src="/ach2.png"
+                    alt="medal_img"
+                    width="40%"
+                    height="40%"
+                  ></Image>
+                </div>
+                <div>
+                  <Image
+                    src="/ach3.png"
+                    alt="medal_img"
+                    width="40%"
+                    height="40%"
+                  ></Image>
+                </div>
+                <div>
+                  <Image
+                    src="/ach4.png"
+                    alt="medal_img"
+                    width="40%"
+                    height="40%"
+                  ></Image>
                 </div>
               </div>
-              <div className={ styles.details_info }>
-                <form className={ styles.details_form }>
-                  <div>
-                    <label>FIRST NAME</label>
-                    <input type="text" placeholder={ context!.data.first_name } maxLength={ 12 } onChange={ () => setSaveBtn( true ) }></input>
-                  </div>
-                  <div>
-                    <label>LAST NAME</label>
-                    <input type="text" placeholder={ context!.data.last_name } maxLength={ 12 } onChange={ () => setSaveBtn( true ) }></input>
-                  </div>
-                  <div>
-                    <label>USERNAME</label>
-                    <input type="text" placeholder={ context!.data.username } maxLength={ 12 } onChange={ () => setSaveBtn( true ) }></input>
-                  </div>
-                </form>
-              </div>
-              <div className={ styles.details_two_factor_aut }>
-                <p className={ styles.two_factor_title }>
-                  TWO-FACTOR AUTHENTICATION
-                </p>
-                <p className={ styles.two_factor_text }>
-                  Two-factor authentication adds an additional layer of security
-                  to your account by requiring more than just a password to sign
-                  in.
-                </p>
-                <label className={ styles.switch }>
-                  <input type="checkbox" onClick={ () => setSwitch( !s_witch ) }></input>
-                  <span className={ cn( styles.slider, styles.round ) }></span>
-                </label>
-                { s_witch && <img src="/QR.png" width="15%" /> }
-              </div>
-              {
-                saveBtn &&
-                <div className={ styles.save_box }>
-                  <div className={ cn( styles_s_l.setting_btn, styles.save_btn ) }>
-                    SAVE
-                  </div>
-                </div>
-              }
             </div>
+            <div className={styles.details_info}>
+              <form className={styles.details_form}>
+                <div>
+                  <label>FIRST NAME</label>
+                  <input type="text" placeholder={data.first_name} maxLength={12} onChange={() => setSaveBtn(true)}></input>
+                </div>
+                <div>
+                  <label>LAST NAME</label>
+                  <input type="text" placeholder={data.last_name} maxLength={12} onChange={() => setSaveBtn(true)}></input>
+                </div>
+                <div>
+                  <label>USERNAME</label>
+                  <input type="text" placeholder={data.username} maxLength={12} onChange={() => setSaveBtn(true)}></input>
+                </div>
+              </form>
+            </div>
+            <div className={styles.details_two_factor_aut}>
+              <p className={styles.two_factor_title}>
+                TWO-FACTOR AUTHENTICATION
+              </p>
+              <p className={styles.two_factor_text}>
+                Two-factor authentication adds an additional layer of security
+                to your account by requiring more than just a password to sign
+                in.
+              </p>
+              <label className={styles.switch}>
+                <input type="checkbox" onClick={() => setSwitch(!s_witch)}></input>
+                <span className={cn(styles.slider, styles.round)}></span>
+              </label>
+              {s_witch && <img src="/QR.png" width="15%" />}
+            </div>
+            {
+              saveBtn &&
+              <div className={styles.save_box}>
+                <div className={cn(styles_s_l.setting_btn, styles.save_btn)}>
+                  SAVE
+                </div>
+              </div>
+            }
+          </div>
         }
       </div>
-    </div> : <></>
+    </div>
   );
 };
 
 export default Profile;
+
+export const getServerSideProps = requireAuthentication(async (context: any) => {
+  return {
+    props: {
+      data: '',
+    }, // will be passed to the page component as props
+  }
+})
