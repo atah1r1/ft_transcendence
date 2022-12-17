4c0fa0a314834595039755edeb6e039f037ff820
hakamdev <ehakam@student.1337.ma>
2022-12-17 20:46:46 +0100

    start impl of game gateway

---
+++ backend/src/game/game.gateway.ts
@@ -0,0 +1,123 @@
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+  WsException,
+} from '@nestjs/websockets';
+import { Socket, Server } from 'socket.io';
+import { AuthService } from 'src/auth/auth.service';
+import { GameService } from './game.service';
+
+const EV_PLAY_AGAINST = 'play_against';
+const EV_PLAY_AGAINST_ACCEPT = 'play_against_accept';
+
+const EV_EMIT_PLAY_AGAINST_REQUEST = 'emit_play_against_request';
+const EV_EMIT_GAME_ID = 'emit_game_id';
+
+@WebSocketGateway({ namespace: 'game', cors: true, origins: '*' })
+export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  constructor(
+    private authService: AuthService,
+    private gameService: GameService,
+  ) {}
+
+  @WebSocketServer() server: Server;
+
+  private async verifyAndSave(client: Socket) {
+    const token: string = client.handshake.auth.token as string;
+    // console.log('token: ', token);
+    const decoded = await this.authService.verifyToken(token);
+    // save the user id in the socket
+    client.data = decoded;
+  }
+
+  async handleConnection(client: Socket) {
+    try {
+      await this.verifyAndSave(client);
+      this.gameService.addConnectedUser(client.data.id, client);
+    } catch (err) {
+      client.disconnect();
+    }
+  }
+
+  handleDisconnect(client: Socket) {
+    this.gameService.removePlayer(client.data.id);
+    this.gameService.removeSpectator(client.data.id);
+    this.gameService.removeConnectedUser(client.data.id, client);
+  }
+
+  validatePlayAgainst(payload: any) {
+    if (!('userId' in payload)) {
+      throw new WsException({
+        error: EV_PLAY_AGAINST,
+        message: 'Invalid payload',
+      });
+    }
+    if (typeof payload.userId !== 'string') {
+      throw new WsException({
+        error: EV_PLAY_AGAINST,
+        message: 'Invalid payload',
+      });
+    }
+  }
+
+  sendPlayAgainstRequestToClient(userId: string, opId: string) {
+    const sockets = this.gameService.getConnectedUserById(userId);
+    sockets.forEach((s) => {
+      s.emit(EV_EMIT_PLAY_AGAINST_REQUEST, {
+        userId: opId,
+      });
+    });
+  }
+
+  sendGameIdToClients(gameId: string) {
+    const players = this.gameService.getGameById(gameId);
+    players.forEach((p) => {
+      const s = this.gameService.getPlayerById(p);
+      if (s) {
+        s.emit(EV_EMIT_GAME_ID, { gameId });
+      }
+    });
+  }
+
+  @SubscribeMessage(EV_PLAY_AGAINST)
+  async playAgainst(client: any, payload: any) {
+    this.validatePlayAgainst(payload);
+    try {
+      const reqAdded = await this.gameService.playAgainst(
+        client.data.id,
+        payload.userId,
+      );
+      if (reqAdded) {
+        this.sendPlayAgainstRequestToClient(client.data.id, payload.userId);
+      }
+    } catch (err) {
+      throw new WsException({
+        error: EV_PLAY_AGAINST,
+        message: err.message,
+      });
+    }
+  }
+
+  @SubscribeMessage(EV_PLAY_AGAINST_ACCEPT)
+  async playAgainstAccept(client: any, payload: any) {
+    this.validatePlayAgainst(payload);
+    try {
+      const gameId = await this.gameService.acceptPlayAgainst(
+        client.data.id,
+        payload.userId,
+        client,
+      );
+      if (gameId) {
+        this.sendGameIdToClients(gameId);
+      }
+    } catch (err) {
+      throw new WsException({
+        error: EV_PLAY_AGAINST,
+        message: err.message,
+      });
+    }
+  }
+}
---
+++ backend/src/game/game.service.ts
@@ -0,0 +1,168 @@
+import { Injectable } from '@nestjs/common';
+import { Socket } from 'socket.io';
+import { UserService } from 'src/user/user.service';
+
+@Injectable()
+export class GameService {
+  constructor(private userService: UserService) {}
+
+  connectedUsers = new Map<string, Socket[]>();
+
+  // gameId => sockets (players and spectators)
+  games = new Map<string, string[]>();
+
+  // userId => socket
+  players = new Map<string, Socket>();
+
+  // userId => socket
+  spectators = new Map<string, Socket>();
+
+  // userId => userId
+  requests = new Map<string, string>();
+
+  getConnectedUsersIds(): string[] {
+    return [...this.connectedUsers.keys()];
+  }
+
+  getConnectedUserById(userId: string): Socket[] {
+    return this.connectedUsers.get(userId) ?? [];
+  }
+
+  getGameById(gameId: string): string[] {
+    return this.games.get(gameId) ?? [];
+  }
+
+  getPlayerById(userId: string): Socket {
+    return this.players.get(userId);
+  }
+
+  addConnectedUser(userId: string, socket: Socket) {
+    const sockets = this.connectedUsers.get(userId);
+    if (sockets) {
+      sockets.push(socket);
+    } else {
+      this.connectedUsers.set(userId, [socket]);
+    }
+  }
+
+  removeConnectedUser(userId: string, socket: Socket) {
+    const sockets = this.connectedUsers.get(userId);
+    if (!sockets) return;
+    const newSockets = sockets.filter((s) => s.id !== socket.id);
+    if (newSockets.length === 0) {
+      this.connectedUsers.delete(userId);
+    } else {
+      this.connectedUsers.set(userId, newSockets);
+    }
+  }
+
+  addPlayer(userId: string, socket: Socket) {
+    if (this.players.has(userId))
+      throw new Error('You are already in a match.');
+    if (this.spectators.has(userId))
+      throw new Error('You are spectating a match.');
+    this.players.set(userId, socket);
+  }
+
+  addSpectator(userId: string, socket: Socket) {
+    if (this.spectators.has(userId))
+      throw new Error('You are already spectating a match.');
+    if (this.players.has(userId)) throw new Error('You are playing a match.');
+    this.spectators.set(userId, socket);
+  }
+
+  addRequest(userId: string, opponentId: string) {
+    if (this.requests.has(userId))
+      throw new Error('You already have a pending request.');
+    if (this.requests.has(opponentId))
+      throw new Error('This user already has a pending request.');
+    this.requests.set(userId, opponentId);
+    this.requests.set(opponentId, userId);
+  }
+
+  removePlayer(userId: string) {
+    this.players.delete(userId);
+  }
+
+  removeSpectator(userId: string) {
+    this.spectators.delete(userId);
+  }
+
+  removeRequest(userId: string, opponentId: string) {
+    this.requests.delete(userId);
+    this.requests.delete(opponentId);
+  }
+
+  getPlayerIds(): string[] {
+    return [...this.players.keys()];
+  }
+
+  getSpectatorIds(): string[] {
+    return [...this.spectators.keys()];
+  }
+
+  async getPlayingFriends(userId: string): Promise<any[]> {
+    const friends = await this.userService.getFriends(userId);
+    if (!friends) return [];
+    const players = friends.filter((friend) => this.players.has(friend.id));
+    return players;
+  }
+
+  async getSpectatingFriends(userId: string): Promise<any[]> {
+    const friends = await this.userService.getFriends(userId);
+    if (!friends) return [];
+    const players = friends.filter((friend) => this.spectators.has(friend.id));
+    return players;
+  }
+
+  async playAgainst(userId: string, otherId: string): Promise<boolean> {
+    if (userId === otherId)
+      throw new Error('You cannot play against yourself.');
+    const blocked = await this.userService.getBlockedUsers(userId);
+    const blockedUsersIds = blocked.map((user) => user.id);
+    if (blockedUsersIds.includes(otherId))
+      throw new Error('You cannot play against this user.');
+    if (this.players.has(userId))
+      throw new Error('You are already in a match.');
+    if (this.spectators.has(userId))
+      throw new Error('You are spectating a match.');
+    if (this.players.has(otherId))
+      throw new Error('This user is already in a match.');
+    if (this.spectators.has(otherId))
+      throw new Error('This user is spectating a match.');
+    this.addRequest(userId, otherId);
+    return true;
+  }
+
+  async startGame(gameId: string) {
+    // TODO: implement game logic
+  }
+
+  async acceptPlayAgainst(
+    userId: string,
+    otherId: string,
+    userSocket: Socket,
+  ): Promise<string> {
+    if (userId === otherId)
+      throw new Error('You cannot play against yourself.');
+    if (this.players.has(userId))
+      throw new Error('You are already in a match.');
+    if (this.spectators.has(userId))
+      throw new Error('You are spectating a match.');
+    if (this.players.has(otherId))
+      throw new Error('This user is already in a match.');
+    if (this.spectators.has(otherId))
+      throw new Error('This user is spectating a match.');
+    if (!this.requests.has(userId))
+      throw new Error('You do not have a pending request.');
+    if (this.requests.get(userId) !== otherId)
+      throw new Error('You do not have a pending request.');
+    this.removeRequest(userId, otherId);
+    this.addPlayer(userId, userSocket);
+    this.addPlayer(otherId, this.connectedUsers.get(otherId)[0]);
+    const gameId = `${userId}_${otherId}`;
+    this.games.set(gameId, [userId, otherId]);
+    await this.startGame(gameId);
+    return gameId;
+  }
+}
